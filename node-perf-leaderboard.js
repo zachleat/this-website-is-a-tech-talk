6ffc0f968b15fd4110c9cf2392656fa55794fb0c
Zach Leatherman <zachleatherman@gmail.com>
2020-10-07 23:14:06 -0500

    Adds benchmark results for 6

--- node-perf-leaderboard.js
+++ node-perf-leaderboard.js
@@ -4,13 +4,13 @@ const fsp = require("fs").promises;
 (async function() {
 
 	let urls = [
+		"https://fervent-beaver-9fea8f.netlify.app/benchmark/6/",
 		"https://fervent-beaver-9fea8f.netlify.app/benchmark/7c/",
 	];
 
 	let results = await PerfLeaderboard(urls, 3);
 	for(let result of results) {
 		let slug = result.url.split("/").filter(entry => !!entry).pop();
-		console.log( slug );
 		await fsp.writeFile(`./_data/benchmark/${slug}.json`, JSON.stringify(result, null, 2));
 	}
 })();
